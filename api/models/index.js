701d9bd1f5cf66d4d5c5b43f4e8adbbdc2142d86
josielvs <josielvs@gmail.com>
2021-07-01 21:17:09 -0300

    Update Ui

--- api/models/index.js
+++
@@ -1,9 +0,0 @@
-const callsModel = require('./dbCallsModel');
-const loginModel = require('./loginModel');
-const amiCallsModel = require('./amiModel');
-
-module.exports = {
-  callsModel,
-  loginModel,
-  amiCallsModel,
-};
