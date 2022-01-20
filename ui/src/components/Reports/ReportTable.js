545742f62878772b9131744f28028fbd3a853730
josielvs <josielvs@gmail.com>
2022-01-20 09:44:03 -0300

    Removendo as gravacoes de user

--- ui/src/components/Reports/ReportTable.js
+++ ui/src/components/Reports/ReportTable.js
@@ -3,44 +3,51 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faBan } from '@fortawesome/free-solid-svg-icons';
 
-class ReportTable extends React.Component {
+const ReportTable = ({ call, role }) => {
 
-  render() {
-    let showRecs = { icon: '', path: '', target: '' };
-    const { call } = this.props;
-    const { data, origem_primaria, origem_segundaria, destino_primario, destino_secundario, setor, aguardando_atendimento, duracao, status, sequencia, tipo, protocolo, tipo_saida, id } = call;
-    let url = window.location.href;
-    url = url.split('/')[2];
-    const date = new Date(data);
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const second = date.getSeconds();
+  let showRecs = { icon: '', path: '', target: '' };
+  // const { call } = this.props;
+  const { data, origem_primaria, origem_segundaria, destino_primario, destino_secundario, setor, aguardando_atendimento, duracao, status, sequencia, tipo, protocolo, tipo_saida, id } = call;  
 
-    const dateBr = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
-    const time = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
-    return (
-      <tbody>
-        <tr className='is-size-7'>
-          <td>{`${dateBr} ${time}`}</td>
-          <td>{origem_primaria}</td>
-          <td>{origem_segundaria}</td>
-          {/* <td>{destino_primario}</td> */}
-          <td>{setor}</td>
-          <td>{destino_secundario}</td>
-          <td>{status}</td>
-          <td>{aguardando_atendimento}</td>
-          <td>{duracao}</td>
-          <td>{tipo}</td>
-          {/* <td>{id}</td>
-          <td>{sequencia}</td> */}
-	        <td><Link to={{ pathname: `http://${url}/api/download/file/${protocolo}` }} target="_blank"><FontAwesomeIcon icon={faPlay} fixedWidth /></Link></td>
-        </tr>
-      </tbody>
-    )
-  }
+  let url = window.location.href;
+  url = url.split('/')[2];
+
+  const date = new Date(data);
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+
+  // const checkUserToIcon = role !== 'user' ? faPlay : faBan;
+  // const checkUserToLink = role !== 'user' ? `http://${url}/api/download/file/${protocolo}` : '#';
+
+  const adminTd = <Link to={{ pathname: `http://${url}/api/download/file/${protocolo}` }} target="_blank"><FontAwesomeIcon icon={faPlay} fixedWidth /></Link>;
+  const userTd = <FontAwesomeIcon icon={faBan} fixedWidth />;
+
+  const dateBr = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
+  const time = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
+
+  return (
+    <tbody>
+      <tr className='is-size-7 has-text-centered '>
+        <td>{`${dateBr} ${time}`}</td>
+        <td>{origem_primaria}</td>
+        <td>{origem_segundaria}</td>
+        {/* <td>{destino_primario}</td> */}
+        <td>{setor}</td>
+        <td>{destino_secundario}</td>
+        <td>{status}</td>
+        <td>{aguardando_atendimento}</td>
+        <td>{duracao}</td>
+        <td>{tipo}</td>
+        {/* <td>{id}</td>
+        <td>{sequencia}</td> */}
+        <td>{ role !== 'admin' ? userTd : adminTd }</td>
+      </tr>
+    </tbody>
+  )
 }
 
 export default ReportTable;
